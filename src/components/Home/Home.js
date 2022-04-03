026f77507c7cea7cc34b3dcd416ad1e835ace843
TW001ChenYI <D10816104@gs.takming.edu.tw>
2022-04-03 17:07:16 +0800

    0403-1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,9 @@ const Home =()=>{
     return(
     <div>
         <Header />
-        {/* <h3>本專案為Dcard應徵實習生作業<br/>
+        <h3>本專案為Dcard應徵實習生作業<br/>
         作者：YI-HAN,CHEN<br/>
         日期：2022/03/10日完成
-        </h3> */}
+        </h3>
     </div>)}
 export {Home}
--- src/components/Notfound/Notfound.js
+++ src/components/Notfound/Notfound.js
@@ -2,7 +2,7 @@ const Notfound = ()=>{
     return(
         <div>
             <br/>
-            <h2>[503]OOPS ,This page under Maintenance</h2> 
+            <h2>OOPS ,This page under Maintenance</h2> 
             <a href="/GitHub_Search_Repos">回首頁</a>
         </div>
     )
